35e82751436f10e69ac22d7deb794600d91fdcff
resikrys <resicris@gmail.com>
2025-05-27 23:23:54 +0200

    fix library class

--- src/main/java/s1_04_testing_lvl1_ex1/Library.java
+++ src/main/java/s1_04_testing_lvl1_ex1/Library.java
@@ -10,38 +10,47 @@ public class Library {
         this.bookList = new ArrayList<>();
     }
 
-    public ArrayList<Book> getBooks() {
-        return (ArrayList<Book>) this.bookList;
+    public List<Book> getBooks() {
+        return this.bookList;
     }
 
     public void addBook(Book book) {
-        this.bookList.add(book);
+        for (Book b : bookList) {
+            if (b.getTitle().equalsIgnoreCase(book.getTitle())) {
+                return; //If exists, scape loop
+            }
+        }
+        bookList.add(book);
+        bookList.sort((b1, b2) -> b1.getTitle().compareToIgnoreCase(b2.getTitle()));
     }
 
     //get book title at specific [i]
-    public void getBookTitleAtIndex(int target) throws Exception {
+    public String getBookTitleAtIndex(int target) throws Exception {
         if (target >= 0 && target < bookList.size()) {
-            System.out.println("Book searched:" + bookList.get(target).getTitle());
+            return bookList.get(target).getTitle();
         } else {
             throw new Exception("The book was not found in the list.");
         }
     }
 
     //add book at specific [i]
-    public void addBookAtIndex(int target, Book book) throws Exception {
-        if (target >= 0 && target < bookList.size()) {
-            bookList.add(target, book);
+    public void addBookAtIndex(int index, Book book) throws Exception {
+        if (index >= 0 && index <= bookList.size()) {
+            bookList.add(index, book);
+            bookList.sort((b1, b2) -> b1.getTitle().compareToIgnoreCase(b2.getTitle()));
         } else {
-            throw new Exception("The book was not added.");
+            throw new Exception("Invalid index.");
         }
     }
 
     public void deleteBookByTitle(String title) throws Exception {
         for (int i = 0; i < bookList.size(); i++) {
-            if (bookList.get(i).getTitle().equals(title)){
+            if (bookList.get(i).getTitle().equals(title)) {
                 bookList.remove(i);
+                return;
             }
         }
+        throw new Exception("Book with title \"" + title + "\" not found.");
     }
 
     public String toString() {
