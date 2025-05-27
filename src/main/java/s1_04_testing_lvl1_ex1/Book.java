5acbcf65f5d0cc7d64bb3d83f4d8cb9baa14dd99
resikrys <resicris@gmail.com>
2025-05-27 23:03:00 +0200

    add book class

---
+++ src/main/java/s1_04_testing_lvl1_ex1/Book.java
@@ -0,0 +1,31 @@
+package s1_04_testing_lvl1_ex1;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+        return "Title: " + this.getTitle() + ", author: " + this.getAuthor();
+    }
+}
