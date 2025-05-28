f5499613b0e0116ea4b6218efafdc0c47cb9fae6
resikrys <resicris@gmail.com>
2025-05-28 17:26:35 +0200

    upd test parameterized

--- src/test/java/s1_04_testing_lvl1_ex2/CalculateDniTest.java
+++ src/test/java/s1_04_testing_lvl1_ex2/CalculateDniTest.java
@@ -1,25 +1,19 @@
 package s1_04_testing_lvl1_ex2;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculateDniTest {
-    private CalculateDni newMatchLetter;
+class CalculateDniTest {
 
-
-    @BeforeEach
-    void setUp() {
-        CalculateDni newMatchLetter = new CalculateDni(2);
+    @ParameterizedTest
+    @CsvSource({
+            "12345678, Z",
+            "11111111, H",
+            "22222222, M"
+    })
+    void testDniLetterCalculator(int num, char assignedLetter) {
+        CalculateDni calc = new CalculateDni(num);
+        assertEquals(assignedLetter, calc.getDniLetter());
     }
-
-    @Test
-    void testLetterMatchWithGivenNumber() {
-        num = newMatchLetter.getNum();
-        dniLetter = newMatchLetter.getDniLetter() = newMatchLetter.getLettersList().charAt(num);
-        assertSame(dniLetter, newMatchLetter.getLettersList().charAt(num), "Correct letter obtained: ");
-    }
-
-
 }
