85e83eb97d7b1938c180e858b198521d834b8ea3
resikrys <resicris@gmail.com>
2025-05-28 17:43:58 +0200

    delete main file

--- src/main/java/s1_04_testing_lvl1_ex2/Main.java
+++
@@ -1,8 +0,0 @@
-package s1_04_testing_lvl1_ex2;
-
-public class Main {
-    public static void main(String[] args) {
-        CalculateDni newMatchLetter = new CalculateDni(2);
-        System.out.println(newMatchLetter.toString());
-    }
-}
